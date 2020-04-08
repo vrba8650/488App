e360b0eb67e6535ec1f0f84886e92bc92fb8b943
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-04-08 11:56:35 -0600

    added quit button in fail screen/ populated settings screen

--- app/src/main/java/com/example/tapsquare/fail.java
+++ app/src/main/java/com/example/tapsquare/fail.java
@@ -2,13 +2,48 @@ package com.example.tapsquare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class fail extends AppCompatActivity {
+    private ImageButton settings;
+    private Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fail);
+
+        //setting button
+        settings = (ImageButton) findViewById(R.id.btnSettings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettingsActivity();
+            }
+        });
+
+        //quit button
+        quit = (Button) findViewById(R.id.quitButton);
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void openSettingsActivity() {
+        Intent intent = new Intent(this, settings.class);
+        startActivity(intent);
     }
 }
