a029b9dd324f1aa4e8ed2f35941ee7ef3e9f2cee
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-04-08 10:45:35 -0600

    Added fail and settings activity

---
+++ app/src/main/java/com/example/tapsquare/fail.java
@@ -0,0 +1,14 @@
+package com.example.tapsquare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fail);
+    }
+}
