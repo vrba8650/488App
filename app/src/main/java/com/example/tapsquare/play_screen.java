03bb98d686da2e36db0cb7943f068a967964fe53
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-04-08 11:32:52 -0600

    added playscreen setting button

--- app/src/main/java/com/example/tapsquare/play_screen.java
+++ app/src/main/java/com/example/tapsquare/play_screen.java
@@ -2,13 +2,28 @@ package com.example.tapsquare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class play_screen extends AppCompatActivity {
-
+    private ImageButton settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
+        settings = (ImageButton) findViewById(R.id.btnSettings);
+        settings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openSettingsActivity();
+            }
+        });
+    }
+    public void openSettingsActivity(){
+        Intent intent = new Intent(this, settings.class);
+        startActivity(intent);
     }
 }
