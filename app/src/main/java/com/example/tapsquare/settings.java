7d7bd96fe93eba1d684946d2423088578d92514f
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-04-10 11:29:55 -0600

    rigged settings buttons

--- app/src/main/java/com/example/tapsquare/settings.java
+++ app/src/main/java/com/example/tapsquare/settings.java
@@ -2,13 +2,44 @@ package com.example.tapsquare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class settings extends AppCompatActivity {
+    private Button returnButton;
+    private Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        returnButton = (Button) findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openReturnActivity();
+            }
+        });
+
+        quit = (Button) findViewById(R.id.quitButton);
+        quit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openReturnActivity(){
+        Intent intent = new Intent(this, play_screen.class);
+        startActivity(intent);
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
