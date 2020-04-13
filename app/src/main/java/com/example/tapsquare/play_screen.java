34f6dc93130270f05b3718a5381dd4beb748aaa3
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-04-13 12:00:04 -0600

    added blue square animation

--- app/src/main/java/com/example/tapsquare/play_screen.java
+++ app/src/main/java/com/example/tapsquare/play_screen.java
@@ -3,19 +3,72 @@ package com.example.tapsquare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class play_screen extends AppCompatActivity {
     private ImageButton settings;
     private Button fail;
+    private int screenWidth;
+
+    //objects
+    private Button blueSq;
+
+
+    //position
+    private float bluePosX;
+
+
+    //initalize
+    private Handler handeler = new Handler();
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
+
+        //blue square
+        blueSq = (Button)findViewById(R.id.blue);
+
+        //get screen size
+        WindowManager wm = getWindowManager();
+        Display disp = wm.getDefaultDisplay();
+        Point size = new Point();
+        disp.getSize(size);
+        screenWidth = size.x;
+
+
+        //start timer
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handeler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changePos();
+                    }
+                });
+            }
+            }, 0, 20);
+
+
+
+
+
+
+
+
 
         //settings button
         settings = (ImageButton) findViewById(R.id.btnSettings);
@@ -49,5 +102,17 @@ public class play_screen extends AppCompatActivity {
     public void openFailActivity(){
         Intent intent = new Intent(this, fail.class);
         startActivity(intent);
+    }
+
+
+    //object movement
+    public void changePos(){
+        //blue square left
+        bluePosX -= 10;
+        if(blueSq.getX() +blueSq.getWidth() < 50){
+            bluePosX += 10;
+
+        }
+
     }
 }
