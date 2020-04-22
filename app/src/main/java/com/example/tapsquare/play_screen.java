f0bc4593db84c45acaaaa72281c6f9890fcdcdac
Evan-Minor <42878572+Evan-Minor@users.noreply.github.com>
2020-04-22 11:39:55 -0600

    testing

--- app/src/main/java/com/example/tapsquare/play_screen.java
+++ app/src/main/java/com/example/tapsquare/play_screen.java
@@ -135,7 +135,7 @@ public class play_screen extends AppCompatActivity {
             successorfail.setText("Winner");
         }
 
-        else if(redSquare.getRight()>blueSquareRightBound && redSquareLeftBound < blueSquareRightBound)
+        else if(redSquare.getRight()<blueSquareRightBound && redSquareLeftBound > blueSquareRightBound)
         {
             successorfail.setText("Winner");
         }
@@ -183,7 +183,7 @@ public class play_screen extends AppCompatActivity {
             blueSquare.setX(blueSquareX);
         }
         blueSquareRightBound = blueSquare.getRight();
-        blueSquareLeftBound = blueSquare.getX()-45;
+        blueSquareLeftBound = blueSquare.getLeft();
 
     }
 
